9850688c593b12aa1fe08a3b5b699893e08ae970
pozhiloyPumba <ivanov.in@phystech.edu>
2021-12-11 17:47:42 +0300

    name for sem

--- IPC/yacht.c
+++ IPC/yacht.c
@@ -13,6 +13,12 @@
 #define NRIDE attractionParams[1]  // limit of rides
 #define NSEAT attractionParams[2]  // num of seats
 
+const int wideTrap = 1;
+const int semPlaceOnYacht = 0;
+const int semBanOfDebarkation = 1;
+const int semTrap = 2;
+const int semBanEnter = 3;
+
 //=====================================================================================================
 
 int captainMode (int sem_id, int countRides, int countSeats);  // ))))
@@ -30,7 +36,7 @@ int main (int argc, char *argv[])
     for (int i = 0; i < 3; ++i) {
         attractionParams[i] = atoi (argv[i + 1]);
         if (attractionParams[i] <= 0) {
-            printf ("You are gave me uncorrect arg number %d\n Please repeat run!\n", attractionParams[i]);
+            printf ("You gave me uncorrect arg number %d\n Please repeat run!\n", attractionParams[i]);
             return 0;
         }
     }
@@ -42,9 +48,9 @@ int main (int argc, char *argv[])
     unsigned short initSemsValue[4] = {0, 0, 0, 0};
     semctl (sem_id, 0, SETALL, initSemsValue);
 
-    struct sembuf landing[] = {{3, 0, 0}, {2, -1, 0}, {0, -1, 0}, {1, 1, 0}};
-    struct sembuf debarkation[] = {{1, 0, 0}, {2, -1, 0}, {0, 1, 0}};
-    struct sembuf freeTrap[] = {{2, 1, 0}};
+    struct sembuf landing[] = {{semBanEnter, 0, 0}, {semTrap, -1, 0}, {semPlaceOnYacht, -1, 0}, {semBanOfDebarkation, 1, 0}};
+    struct sembuf debarkation[] = {{semBanOfDebarkation, 0, 0}, {semTrap, -1, 0}, {semPlaceOnYacht, 1, 0}};
+    struct sembuf freeTrap[] = {{semTrap, 1, 0}};
 
     for (int i = 0; i < NPASS; ++i) {
         int pid = fork ();
@@ -64,11 +70,13 @@ int main (int argc, char *argv[])
                     return 0;
                 }
 
-                printf ("\t\t[%d] set on yacht\n", getpid ());
+                printf ("\t\t[%d] set on trap\n", getpid ());
                 semop (sem_id, freeTrap, 1);
+                printf ("\t\t[%d] got off from trap to yacht\n", getpid ());
                 semop (sem_id, debarkation, 3);
-                printf ("\t\t[%d] got off the yacht\n", getpid ());
+                printf ("\t\t[%d] got off from the yacht to trap\n", getpid ());
                 semop (sem_id, freeTrap, 1);
+                printf ("\t\t[%d] got off from the trap to Earth. Finally\n", getpid ());
             }
 
             //-----------------------------------------------------------------------------------------------------
@@ -98,12 +106,12 @@ int captainMode (int sem_id, int countRides, int countSeats)
         return -1;
     }
 
-    if (captPid) {  // place on yacht; ban of debarcation; trap
+    if (captPid) {  // place on yacht; ban of debarcation; trap; ban of enter
 
-        struct sembuf initLanding[] = {{0, countSeats, 0}, {2, 1, 0}};
-        struct sembuf waitLanding[] = {{0, 0, 0}, {3, 1, 0}, {2, -1, 0}};
-        struct sembuf endOfTour[] = {{1, -countSeats, 0}, {2, 1, 0}};
-        struct sembuf waitEscapePass[] = {{0, -countSeats, 0}, {3, -1, 0}, {2, -1, 0}};
+        struct sembuf initLanding[] = {{semPlaceOnYacht, countSeats, 0}, {semTrap, wideTrap, 0}};
+        struct sembuf waitLanding[] = {{semPlaceOnYacht, 0, 0}, {semBanEnter, 1, 0}, {semTrap, -wideTrap, 0}};
+        struct sembuf endOfTour[] = {{semBanOfDebarkation, -countSeats, 0}, {semTrap, wideTrap, 0}};
+        struct sembuf waitEscapePass[] = {{semPlaceOnYacht, -countSeats, 0}, {semBanEnter, -1, 0}, {semTrap, -wideTrap, 0}};
 
         for (int i = 0; i < countRides; ++i) {
             printf ("Captain announced [%d] landing:\n", i + 1);
@@ -111,7 +119,6 @@ int captainMode (int sem_id, int countRides, int countSeats)
             printf ("\tCaptain: i'm waiting\n");
 
             semop (sem_id, waitLanding, 3);
-            usleep (5);  // for be on time printf("\t[%d] set on yacht\n", getpid()); (maybe don't need)
             printf ("\tCaptain: land is out. We are leaving\n");
 
             printf ("POEXALI))))\t");  // this part for fun
