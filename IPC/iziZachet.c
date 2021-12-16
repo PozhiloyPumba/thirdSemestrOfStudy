cf45e2621d50ed5b10f57eeba9842c12b2896318
pozhiloyPumba <ivanov.in@phystech.edu>
2021-12-16 16:13:07 +0300

    usefull commit

--- IPC/iziZachet.c
+++ IPC/iziZachet.c
@@ -30,7 +30,7 @@ int main (int argc, char *argv[])
     struct sembuf begin[] = {{0, 1, 0}};
     struct sembuf end[] = {{0, -1, 0}};
     key_t key;
-    if ((key = ftok ("./iziZachet.c", 'a')) == (key_t) (-1)) {
+    if ((key = ftok ("./iziZachet.c", 'a')) == (key_t)(-1)) {
         perror ("IPC error: ftok");
         exit (1);
     }
